07a8b941fc54e6672a8967170affe22e3bc89238
Ari Effendi <ari@arped.id>
2024-01-24 05:06:49 -0500

    Register AdminSeeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->password = Hash::make('123456');
+        $user->save();
+    }
+}
