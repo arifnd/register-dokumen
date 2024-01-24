3267c7a36c69c1296099a28c649b667eeb5b8084
Ari Effendi <ari@arped.id>
2024-01-24 17:22:39 -0500

    Change brandName

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,7 +32,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Green,
             ])
             ->topNavigation()
-            ->brandName('Register Surat')
+            ->brandName('Register Dokumen')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
