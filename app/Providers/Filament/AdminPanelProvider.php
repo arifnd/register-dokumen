3c407be11a8cf5b112ec9911e5b26ac5e764c855
Ari Effendi <ari@arped.id>
2024-01-24 04:41:26 -0500

    - Add profile
    - Set brand name
    
    - Set top navigation

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,12 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('')
             ->login()
+            ->profile()
             ->colors([
                 'primary' => Color::Green,
             ])
+            ->topNavigation()
+            ->brandName('Register Surat')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
